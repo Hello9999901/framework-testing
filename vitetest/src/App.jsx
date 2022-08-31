b6e3069241b0a606b7b8b3917f924fc21555fe33
Hello9999901 <byran8855@gmail.com>
2022-08-31 09:39:40 -0400

    standard-cra and standard-next added

--- vitetest/src/App.jsx
+++ vitetest/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <h1 className='text-black font-black text-7xl'>BOLD TEXT</h1>
-      <div className='absolute w-[1000px] h-[1000px] rounded-full -ml-20 -right-[300px] bg-gradient-to-tr from-red-500 to-[#9A1D2E]'/>
     </div>
   )
 }
