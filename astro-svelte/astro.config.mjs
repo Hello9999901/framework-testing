4cda15b3d690d7517268e4ecb8861a868b9d3dad
Hello9999901 <byran8855@gmail.com>
2022-09-26 21:55:13 -0400

    added astro-svelte

---
+++ astro-svelte/astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), svelte(), tailwind(), sitemap()]
+});
